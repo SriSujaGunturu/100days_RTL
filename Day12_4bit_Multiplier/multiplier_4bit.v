abb107c7e1769d2ea3158ce0ae816cdb43d1d0ef
SriSujaGunturu <143597011+SriSujaGunturu@users.noreply.github.com>
2025-05-23 11:39:18 +0530

    Update

--- Day12_4bit_Multiplier/multiplier_4bit.v
+++ Day12_4bit_Multiplier/multiplier_4bit.v
@@ -92,3 +92,18 @@
 
 	 
 endmodule
+
+module ha (input a,b, 
+	   output sum,carry);
+	assign sum=a^b;
+	assign carry=a&b;
+
+endmodule
+
+module full_adder( input a,b,cin
+		  output sum,cout);
+	assign sum=a^b^c;
+	assign carry=(a&b) | (b&cin) | (cin&a);
+endmodule
+
+	
